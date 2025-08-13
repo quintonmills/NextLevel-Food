47a85eb4d1ff1318abfff27503e6ba3fe8c4ff12
quintonmills <quintonmills98@gmail.com>
2025-08-13 12:17:45 -0400

    implement main pages and css modules

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,3 @@
+export default function MealDeatailsPage() {
+  return <h1>Meal Details</h1>;
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to NextLevel Food</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
+      <p>
+        <Link href="/meals">Meals</Link>
+      </p>
+      <p>
+        <Link href="/meals/share">Share Meals</Link>
+      </p>
+      <p>
+        <Link href="/community">Community</Link>
+      </p>
     </main>
   );
 }
