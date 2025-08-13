74f630d704d68079449f9a6a283f2be1840bea1a
quintonmills <quintonmills98@gmail.com>
2025-08-13 12:18:19 -0400

    extract main-header component

--- app/page.js
+++ app/page.js
@@ -1,20 +1,18 @@
-import Link from "next/link";
-
+import classes from "./page.module.css";
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Time to get started!
-      </h1>
-      <p>
-        <Link href="/meals">Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/share">Share Meals</Link>
-      </p>
-      <p>
-        <Link href="/community">Community</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}></div>
+        <div>
+          <div className={classes.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world</p>
+          </div>
+          <div className={classes.cta}></div>
+        </div>
+      </header>
+      <main></main>
+    </>
   );
 }
