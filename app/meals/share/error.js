8eca4f79bd5f2c2a4dc9cbb0f644d604c954fc85
quintonmills <quintonmills98@gmail.com>
2025-08-14 10:32:10 -0400

    implement storing data into databse

--- app/meals/share/error.js
+++ app/meals/share/error.js
@@ -3,7 +3,7 @@ export default function Error({ error }) {
   return (
     <main className="error">
       <h1>An error occured</h1>
-      <p>Failed to fetch meal data. Please try again later.</p>
+      <p>Failed to create meal</p>
     </main>
   );
 }
