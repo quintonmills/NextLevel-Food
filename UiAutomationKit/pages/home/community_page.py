c977e76e1dac567144ab10318b06f33b3edbd509
quintonmills <quintonmills98@gmail.com>
2025-08-26 16:36:38 -0400

    refactor(CommunityPage): improve readability with dedicated locators and methods

--- UiAutomationKit/pages/home/community_page.py
+++ UiAutomationKit/pages/home/community_page.py
@@ -1,12 +1,19 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class CommunityPage():
     def __init__(self, driver):
         self.driver = driver
+    
+    #locators
+    browse_meals_link = "/html/body/header[1]/nav/ul/li[1]/a"
+
+    def get_browse_meals_link(self):
+        return self.driver.find_element(By.XPATH, self.browse_meals_link)
+    
+    def click_browse_meals_link(self):
+        self.get_browse_meals_link().click()
 
     def  navigate_to_community_recipes(self):
-        browseMeals = self.driver.find_element(By.XPATH, "/html/body/header[1]/nav/ul/li[1]/a")
-        browseMeals.click()
+        self.click_browse_meals_link()
 
         
